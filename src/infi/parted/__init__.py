9a95ca055c4e59e5574d3d060149156173baf71b
Guy Rozendorn <guy@rzn.co.il>
2011-11-30 15:02:46 +0200

    parted prints error to stdout and not stderr

--- src/infi/parted/__init__.py
+++ src/infi/parted/__init__.py
@@ -44,7 +44,7 @@ def execute_parted(args):
     parted.wait()
     if parted.get_returncode() != 0:
         raise PartedRuntimeError(parted.get_returncode(),
-                                 _get_parted_error_message_from_stderr(parted.get_stderr()))
+                                 _get_parted_error_message_from_stderr(parted.get_stdout()))
     return parted.get_stdout()
 
 
