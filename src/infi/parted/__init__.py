0c4272a734dc099704f369005d9f09d899796fae
Guy Rozendorn <guy@rzn.co.il>
2011-11-30 16:07:05 +0200

    fix for format_partition

--- src/infi/parted/__init__.py
+++ src/infi/parted/__init__.py
@@ -141,7 +141,13 @@ class Disk(object):
 
     def format_partition(self, partition_number, filesystem_name, mkfs_options={}):
         """currently mkfs_options is ignored"""
-        self.execute_parted(["mkfs", str(partition_number), filesystem_name])
+        try:
+            self.execute_parted(["mkfs", str(partition_number), filesystem_name])
+        except PartedRuntimeError, error:
+            if "not implemented yet" in error.get_error_message():
+                pass
+            else:
+                raise
         self.force_kernel_to_re_read_partition_table()
         partition_access_path = self._get_partition_acces_path_by_name(partition_number)
         self._execute_mkfs(filesystem_name, partition_access_path)
